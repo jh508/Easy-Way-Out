0e7270a288ebd81618ff2e42a19e01b294aaa498
Joe <62161622+jh508@users.noreply.github.com>
2023-11-12 20:29:00 +0000

    Processing serverCommands

--- media/lua/server/suicide.lua
+++ media/lua/server/suicide.lua
@@ -1,19 +1,10 @@
 
--- Function to check for the key press and kill the player
-function checkForKeyPress()
-    local player = getSpecificPlayer(0) -- Get the player
-    local key1 = 37  -- Key code for the key you want to check (e.g., 25 for "K")
-    local key2 = 50  -- Key code for the key you want to check (e.g., 25 for "M")
-    local key3 = 31  -- Key code for the key you want to check (e.g., 25 for "S")
-
-    if isKeyDown(key1) and isKeyDown(key2) and isKeyDown(key3) and not player:isDead() then
-        player:Say("testing")
+function OnServerCommand(module, command, arguments)
+    if module == "test" then
+        if command == "command" then
+            print(arguments[1])
+        end
     end
 end
 
-
-
-
-
--- Hook the function to the game's update loop
-Events.OnTick.Add(checkForKeyPress)
+Events.OnServerCommand.Add(OnServerCommand)
