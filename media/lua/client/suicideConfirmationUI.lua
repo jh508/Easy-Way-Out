1f44f57c9cbc0300811fd2d9a2c109bd040e0ce1
Joe <62161622+jh508@users.noreply.github.com>
2023-11-12 20:27:30 +0000

    Code cleanup, implemented closePanel function

--- media/lua/client/suicideConfirmationUI.lua
+++ media/lua/client/suicideConfirmationUI.lua
@@ -2,46 +2,68 @@
 require "ISUI/ISPanel"
 
 -- Create a class for your custom UI panel
-MyCustomUIPanel = ISPanel:derive("Kaleerie's Easy Way Out Confirmation Panel")
-
+suicideConfirmationPanel = ISPanel:derive("Kaleerie's Easy Way Out Confirmation Panel")
+-- Global player variable
+local player = getSpecificPlayer(0)
 
 -- Constructor for your custom panel
-function MyCustomUIPanel:initialise()
+function suicideConfirmationPanel:initialise()
     ISPanel.initialise(self)
     self.title = "Easy Way Out Confirmation"
     self.width = 300
     self.height = 200
 
-
-        -- Create the first button
-        self.button1 = ISButton:new(50, 50, 200, 40, "Yes", self, MyCustomUIPanel.onButton1Click)
-        self:addChild(self.button1)
+    -- Create the first button
+    self.button1 = ISButton:new(50, 50, 200, 40, "Yes", self, suicideConfirmationPanel.onButton1Click)
+    self:addChild(self.button1)
     
-        -- Create the second button
-        self.button2 = ISButton:new(50, 100, 200, 40, "No", self, MyCustomUIPanel.onButton2Click)
-        self:addChild(self.button2)
+    -- Create the second button
+    self.button2 = ISButton:new(50, 100, 200, 40, "No", self, suicideConfirmationPanel.onButton2Click)
+    self:addChild(self.button2)
+
+    -- Set the visibility to false by default
+    self:setVisible(false)
 
 end
+
+    -- Create a panel
+    panel = suicideConfirmationPanel:new(100, 100, 300, 200)
 
 -- Function to handle the first button click
-function MyCustomUIPanel:onButton1Click()
-    local player = getSpecificPlayer(0)
-    player:setHealth(0)
-end
+    function suicideConfirmationPanel:onButton1Click()
+        player:setHealth(0)
+        closeSuicideConfirmPanel()
+    end
 
--- Function to handle the second button click
-function MyCustomUIPanel:onButton2Click()
-    
-end
+    -- Function to handle the second button click
+    function suicideConfirmationPanel:onButton2Click()
+        closeSuicideConfirmPanel()
+    end
+
+    function closeSuicideConfirmPanel()
+        panel:setVisible(false)
+    end
 
 
--- Function to open your custom UI panel
-function openCustomUIPanel()
-    local panel = MyCustomUIPanel:new(100, 100, 300, 200)
-    panel:initialise()  -- Call the initialise function to set up the panel
-    panel:addToUIManager()
-    panel:setVisible(true)
-end
+    -- Function to open your custom UI panel
+    function openCustomUIPanel()
+
+        panel:initialise()  -- Call the initialise function to set up the panel
+        panel:addToUIManager()
+        panel:setVisible(true)
+        sendServerCommand("test", "command", {})
+    end
+
+-- Function to check for the key press and kill the player
+    function checkForKeyPress()
+        local key1 = 67  -- Key code for the key you want to check (e.g., 67 for "F9")
+
+        if isKeyDown(key1) then
+            openCustomUIPanel()
+        end
+
+
+    end
 
 -- Register an event to open your panel (e.g., when the game starts)
-Events.OnGameStart.Add(openCustomUIPanel)
+Events.OnTick.Add(checkForKeyPress)
