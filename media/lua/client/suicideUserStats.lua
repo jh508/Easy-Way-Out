13d7458210055495d38fd878a3a7cb58e45531b3
Joe <62161622+jh508@users.noreply.github.com>
2023-11-14 13:41:13 +0000

    Update suicideUserStats.lua

--- media/lua/client/suicideUserStats.lua
+++ media/lua/client/suicideUserStats.lua
@@ -1,3 +1,17 @@
 local userStats = {
     originalStress = 0
 }
+
+local depressingQuotes = {
+    "I've been having dark thoughts recently...",
+    "I'm not sure I can go on much longer...",
+    "This is not worth it anymore...",
+    "Maybe I'm a better zombie than I am survivor..."
+}
+
+local lovelyQuotes = {
+    "Maybe life is worth living...",
+    "I mean, killing zombies is pretty fun...",
+    "What am I even talking about? I love life...",
+    "I'm very happy..."
+}
