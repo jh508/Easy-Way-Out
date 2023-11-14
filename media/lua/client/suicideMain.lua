fef5b7c90d38afd4c9dd9e2ce4742f8fb3576faa
Joe <62161622+jh508@users.noreply.github.com>
2023-11-14 14:58:16 +0000

    Added comments

--- media/lua/client/suicideMain.lua
+++ media/lua/client/suicideMain.lua
@@ -1,10 +1,14 @@
 -- suicideMain.lua
 
+-- table instantiation to be accessed from other scripts
 local activateSuicide = {}
 
+-- Quote that will be displayed if the boolean value "denied" is false
 activateSuicide.deniedQuote = "If only I had a weapon..."
+-- Boolean value to determine whether or not the player can take the easy way out
 activateSuicide.denied = false
 
+-- a function inside the activateSuicide table which checks if the player is holding a weapon
 function activateSuicide.killPlayer()
     local player = getSpecificPlayer(0)
     if player:getPrimaryHandItem() and player:getPrimaryHandItem():IsWeapon() and player:getPrimaryHandItem():getDisplayCategory() == "Weapon" then
