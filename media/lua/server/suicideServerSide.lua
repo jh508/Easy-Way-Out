c548472cde5b08baae203227ea38b97a3f09264e
Joe <62161622+jh508@users.noreply.github.com>
2023-11-15 18:37:05 +0000

    Added support for melee weapons, changed file name

--- media/lua/server/suicideServerSide.lua
+++ media/lua/server/suicideServerSide.lua
@@ -5,11 +5,21 @@ local function OnClientCommand(module, command, player, args)
         local playerOnlineID = player:getOnlineID()
         local commandArgs = {}
         commandArgs[1] = playerOnlineID;
-        
         addSound(player, player:getCurrentSquare():getX(), player:getCurrentSquare():getY(), player:getCurrentSquare():getZ(), 30, 1.0)
         
         for index = 0, players:size() - 1 do
             sendServerCommand(players:get(index), "suicideModule", "deathByFireArm", commandArgs)
+        end
+    else
+        if module == "suicideModule" and command == "deathByMelee" then
+            local players = getOnlinePlayers()
+            local playerOnlineID = player:getOnlineID()
+            local commandArgs = {}
+            commandArgs[1] = playerOnlineID;
+
+            for index = 0, players:size() - 1 do
+                sendServerCommand(players:get(index), "suicideModule", "deathByMelee", commandArgs)
+            end
         end
     end
 end
