339c77fedd3abebc48f814447276784106b4458c
Joe <62161622+jh508@users.noreply.github.com>
2023-11-15 14:07:35 +0000

    Added communication between the server and client to update every online players client

--- media/lua/server/suicide.lua
+++ media/lua/server/suicide.lua
@@ -1,8 +1,17 @@
 
 local function OnClientCommand(module, command, player, args)
-    local commandSenderSteamID = player:getSteamID()
+    local players = getOnlinePlayers()
+    local playerOnlineID = player:getOnlineID()
+    local commandArgs = {}
+    commandArgs[1] = playerOnlineID;
 
+    if module == "suicideModule" and command == "deathByFireArm" then
+        for index = 0, players:size() - 1 do
+            sendServerCommand(players:get(index), "suicideModule", "deathByFireArm", commandArgs)
+        end
+    end
 end
+
 
 -- Triggered event when the client sends a command to the server
 Events.OnClientCommand.Add(OnClientCommand)
