eade5e1ebe2dce2c6e0226ec7b41a1a440497c6b
Joe <62161622+jh508@users.noreply.github.com>
2023-11-16 12:40:51 +0000

    Fixed an issue where blood would render below every client's player if a user on the server quick resets

--- media/lua/client/suicideMain.lua
+++ media/lua/client/suicideMain.lua
@@ -1,40 +1,46 @@
--- suicideMain.lua
+-- quickResetMain.lua
 
 -- table instantiation to be accessed from other scripts
-local activateSuicide = {}
+local activateQuickReset = {}
 
 -- Quote that will be displayed if the boolean value "denied" is false
-activateSuicide.deniedQuote = "If only I had a weapon..."
+activateQuickReset.deniedQuote = "If only I had a weapon..."
 -- Boolean value to determine whether or not the player can take the easy way out
-activateSuicide.isDenied = true
+activateQuickReset.isDenied = true
+
+-- Module and Command variables
+local moduleName = "resetModule"
+local deathByFireArmCommand = "deathByFireArm"
+local deathByMeleeCommand = "deathByMelee"
 
 -- Sound variables
 local deathByFireArmSound = "9mmShot"
 local deathByMeleeSound = "PZ_HeadExtract_01"
 
-local function handlePlayerDeath(command, soundFile, onlineID)
+local function handlePlayerDeath(command, soundFile, onlineID, xPos, yPos, zPos)
     local player = getSpecificPlayer(0)
-    addBloodSplat(player:getCurrentSquare(), 200)
+    addBloodSplat(getSquare(xPos, yPos, zPos), 200)
     getSoundManager():PlayWorldSound(soundFile, true, player:getCurrentSquare(), 0, 4, 1, false)
     if player:getOnlineID() == onlineID then
         player:setHealth(0)
     end
 end
 
--- a function inside the activateSuicide table which checks if the player is holding a weapon
-function activateSuicide.killPlayer()
+-- a function inside the activateQuickReset table which checks if the player is holding a weapon
+function activateQuickReset.killPlayer()
     local player = getSpecificPlayer(0)
     local playerPrimaryHandItem = player:getPrimaryHandItem()
     -- Get the sound manager and play the audio "9mmShot" at the players current location
     if playerPrimaryHandItem and playerPrimaryHandItem:IsWeapon() and playerPrimaryHandItem:getDisplayCategory() == "Weapon" then
         if playerPrimaryHandItem:isAimedFirearm() then
             if playerPrimaryHandItem:getCurrentAmmoCount() > 0 then
+                -- If TRUE, the player is hosting the server in multiplayer
                 if isClient() then
-                    sendClientCommand("suicideModule", "deathByFireArm", {})
+                    sendClientCommand(moduleName, deathByFireArmCommand, {})
                     playerPrimaryHandItem:setCurrentAmmoCount(playerPrimaryHandItem:getCurrentAmmoCount() - 1)
-                    activateSuicide.isDenied = false
+                    activateQuickReset.isDenied = false
                     return
-                else
+                else -- Or the player is playing singleplayer
                     addBloodSplat(player:getCurrentSquare(), 200)
                     getSoundManager():PlayWorldSound(deathByFireArmSound, true, player:getCurrentSquare(), 0, 4, 1, false)
                     playerPrimaryHandItem:setCurrentAmmoCount(playerPrimaryHandItem:getCurrentAmmoCount() - 1)
@@ -45,36 +51,34 @@ function activateSuicide.killPlayer()
                 player:Say("If I'm going to do this... I'll need a bullet")
             end
         else
-            -- If TRUE, the player is hosting the server in singleplayer
+            -- If TRUE, the player is hosting the server in multiplayer
             if isClient() then
-                getSoundManager():PlayWorldSound(deathByMeleeSound, true, player:getCurrentSquare(), 0, 4, 1, false)
-                addBloodSplat(player:getCurrentSquare(), 200)
-                sendClientCommand("suicideModule", "deathByMelee", {})
-                activateSuicide.isDenied = false
-            else
+                sendClientCommand(moduleName, deathByMeleeCommand, {})
+                activateQuickReset.isDenied = false
+            else -- Or the player is playing singleplayer
                 getSoundManager():PlayWorldSound(deathByMeleeSound, true, player:getCurrentSquare(), 0, 4, 1, false)
                 addBloodSplat(player:getCurrentSquare(), 200)
                 player:setHealth(0)
-                activateSuicide.isDenied = false
+                activateQuickReset.isDenied = false
             end
         end
     else
-        activateSuicide.isDenied = true
-        player:Say(activateSuicide.deniedQuote)
+        activateQuickReset.isDenied = true
+        player:Say(activateQuickReset.deniedQuote)
     end
 end
 
 local function OnServerCommand(module, command, arguments)
-    if module == "suicideModule" then
-        if command == "deathByFireArm" then
-            handlePlayerDeath(command, deathByFireArmSound, arguments[1])
-        elseif command == "deathByMelee" then
-            handlePlayerDeath(command, deathByMeleeSound, arguments[1])
+    if module == moduleName then
+        if command == deathByFireArmCommand then
+            handlePlayerDeath(command, deathByFireArmSound, arguments.onlineID, arguments.x, arguments.y, arguments.z)
+        elseif command == deathByMeleeCommand then
+            handlePlayerDeath(command, deathByMeleeSound, arguments.onlineID, arguments.x, arguments.y, arguments.z)
         end
     end
 end
 
 Events.OnServerCommand.Add(OnServerCommand)
 
-return activateSuicide
+return activateQuickReset
 
